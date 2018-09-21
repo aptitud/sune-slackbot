2b8426a56a19fb89fb18b560d7c04217c877e151
Johan Elmström <johan.elmstrom@mrgreen.com>
2018-09-21 16:33:00 +0200

    sune docs

--- handlers/sune.js
+++ handlers/sune.js
@@ -6,7 +6,7 @@ module.exports = tiny => {
     if(command === 'hjälp') {
         send('Hej. \n Jag kan hjälpa dig med att leta i trello om du skriver `trello` och något du letar efter. Eller så kan jag deploya om apitud.se. \n Om det inte räcker, -> https://github.com/aptitud/sune-slackbot ') 
     } else if (command === 'ls'){
-       send('`trello [command]` \t:söker i trello efter [command] \n `netlify deploy` \t: deployar om aptitud.se') 
+       send('`trello [command]`\t: Söker i trello efter [command] \n `netlify deploy`\t\t: Deployar om aptitud.se') 
     
     }else {
       send(`[${match[1]}] vet jag inte vad jag ska göra med. Jag kan bara deploy`);
